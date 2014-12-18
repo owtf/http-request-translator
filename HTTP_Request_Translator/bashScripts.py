efebc9c6a1c02a1bcf61ddca73d4005f091c0c65
cjdupreez <cjrdupreez@gmail.com>
2014-12-18 10:13:33 -0500

    Fixed regex parsing of url to be grabbed.

--- HTTP_Request_Translator/bashScripts.py
+++ HTTP_Request_Translator/bashScripts.py
@@ -5,12 +5,14 @@ import re
 def generate_script(header_dict, details_dict, searchString=None):
 	
 	method = details_dict['method'].strip()
-	host = details_dict['Host']
-	headers = str(header_dict)
-	a = re.sub(r"{|}","",headers)
-	b = re.sub(r"': '"," : ",a)
-	c = re.sub(r"', '","\" --header \"",b)
-	formattedHeaders = re.sub(r"\'","\"",c)
+        host = details_dict['Host']
+        headers = str(header_dict)
+        a = re.sub(r"{|}","",headers)
+        b = re.sub(r"': '"," : ",a)
+        c = re.sub(r"', '","\" --header \"",b)
+        d = re.sub(r"\'","\"",c)
+        e = re.sub(r"\"Host :","",d)
+        formattedHeaders = re.sub(r"\"","",e,1)
 
 	if searchString:
 		try:
@@ -19,24 +21,24 @@ def generate_script(header_dict, details_dict, searchString=None):
 			        if method == 'POST':
 					skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request POST --header '''+formattedHeaders+''' --include
+curl -v --request POST'''+formattedHeaders+''' --include
 					'''
 			        elif method == 'GET':
 					skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request GET --header '''+formattedHeaders+''' --include > request
+curl -v --request GET'''+formattedHeaders+''' --include > request
 replacement = $(tput setaf 1; echo "'''+searchString+'''")
 echo ${request//'''+searchString+'''/$replacement}
 					'''
 			        elif method == 'PUT':
 					skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request PUT --header '''+formattedHeaders+''' --include
+curl -v --request PUT'''+formattedHeaders+''' --include
 					'''
 			        elif method == 'DELETE':
 					skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request DELETE --header '''+formattedHeaders+''' --include
+curl -v --request DELETE'''+formattedHeaders+''' --include
 					'''
 			
 			else:
@@ -44,22 +46,22 @@ curl -v --request DELETE --header '''+formattedHeaders+''' --include
 				if method == 'POST':
                                         skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request POST --header '''+formattedHeaders+''' --include
+curl -v --request POST'''+formattedHeaders+''' --include
 					'''
                                 elif method == 'GET':
                                         skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request GET --header '''+formattedHeaders+''' --include
+curl -v --request GET'''+formattedHeaders+''' --include
 					'''
                                 elif method == 'PUT':
                                         skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request PUT --header '''+formattedHeaders+''' --include
+curl -v --request PUT'''+formattedHeaders+''' --include
 					'''
                                 elif method == 'DELETE':
                                         skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request DELETE --header '''+formattedHeaders+''' --include
+curl -v --request DELETE'''+formattedHeaders+''' --include
 					'''
 		except IndexError as i :
 			print "You haven't given the port Number"
@@ -72,44 +74,44 @@ curl -v --request DELETE --header '''+formattedHeaders+''' --include
                                 if method == 'POST':
                                        	skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request POST --header '''+formattedHeaders+''' --include
+curl -v --request POST'''+formattedHeaders+''' --include
 					'''
                                 elif method == 'GET':
                                         skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request GET --header '''+formattedHeaders+''' --include
+curl -v --request GET'''+formattedHeaders+''' --include
 					'''
                                 elif method == 'PUT':
                                         skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request PUT --header '''+formattedHeaders+''' --include
+curl -v --request PUT'''+formattedHeaders+''' --include
 					'''
                                 elif method == 'DELETE':
                                         skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request DELETE --header '''+formattedHeaders+''' --include
+curl -v --request DELETE'''+formattedHeaders+''' --include
 					'''
 
 			else:
 				if method == 'POST':
                                         skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request POST --header '''+formattedHeaders+''' --include
+curl -v --request POST'''+formattedHeaders+''' --include
 					'''
                                 elif method == 'GET':
                                         skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request GET --header '''+formattedHeaders+''' --include
+curl -v --request GET'''+formattedHeaders+''' --include
 					'''
                                 elif method == 'PUT':
                                         skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request PUT --header '''+formattedHeaders+''' --include
+curl -v --request PUT'''+formattedHeaders+''' --include
 					'''
                                 elif method == 'DELETE':
                                         skeleton_code = '''\
 #!/usr/bin/env bash
-curl -v --request DELETE --header '''+formattedHeaders+''' --include
+curl -v --request DELETE'''+formattedHeaders+''' --include
 					'''
 		except IndexError as i :
 			print "You haven't given the port Number"
