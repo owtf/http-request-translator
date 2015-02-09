8b925f762791175cd6bc8d550b78596f382c629a
cjdupreez <cjrdupreez@gmail.com>
2015-02-09 17:17:16 -0500

    Started putting together the php script output...

--- HTTP_Request_Translator/phpScripts.py
+++ HTTP_Request_Translator/phpScripts.py
@@ -1,10 +1,78 @@
 #!/usr/bin/python
+import pprint
+import re
 
-def generate_script(header_dict, details_dict):
+def generate_script(header_dict, details_dict, searchString=None):
 	
-	''' This method contains the skeleton code of php script to send requests '''
-	skeleton_code = "<Insert the skeleton_code here>"
-	''' Take in the values from the parsed_dictionary and then add the parsed values to this to make a basic script
-	'''
-	return skeleton_code
-	
+	method = details_dict['method'].strip()
+        host = details_dict['Host']
+        headers = str(header_dict)
+
+	if searchString:
+		try:
+			if not 'proxy' in details_dict:
+ 
+			        skeleton_code = '''\
+<?php
+$curl = curl_init();
+curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => '''+"'"+host+"',"+'''
+));
+$response = curl_exec($curl);
+curl_close($curl);
+return $response
+?>'''
+			
+			else:
+
+				skeleton_code = '''\
+<?php
+$curl = curl_init();
+curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => '''+"'"+host+"',"+'''
+));
+$response = curl_exec($curl);
+curl_close($curl);
+return $response
+?>'''
+
+		except IndexError as i :
+			print "You haven't given the port Number"
+		else :
+                       	print (skeleton_code)
+	else :
+		try :
+                        if not 'proxy' in details_dict :
+
+				skeleton_code = '''\
+<?php
+$curl = curl_init();
+curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => '''+"'"+host+"',"+'''
+));
+$response = curl_exec($curl);
+curl_close($curl);
+return $response
+?>'''
+
+			else:
+				skeleton_code = '''\
+<?php
+$curl = curl_init();
+curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => '''+"'"+host+"',"+'''
+));
+$response = curl_exec($curl);
+curl_close($curl);
+return $response
+?>'''
+
+		except IndexError as i :
+			print "You haven't given the port Number"
+
+                else :
+			print (skeleton_code)
