f2e42757aef5e0aa100d19074098f4a21ec964f0
Tao Sauvage <sauvage.tao@gmail.com>
2015-07-17 21:31:29 +0200

    Quick cleaning of pythonScripts.py

--- http_request_translator/pythonScripts.py
+++ http_request_translator/pythonScripts.py
@@ -1,46 +1,44 @@
-#!/usr/bin/python
+from __future__ import print_function
 
-from urllib import quote
 import sys
+try:
+    from urllib import quote
+except ImportError:
+    from urllib.parse import quote
 
 try:
     from termcolor import colored
 except ImportError:
-    print "Dependency of the library 'termcolor' not satisfied. \
-    Do $pip install termcolor to install the library :-) "
-    sys.exit(0)
+    print("Dependency of the library 'termcolor' not satisfied. Do $pip install termcolor to install the library :-)")
+    sys.exit(-1)
 
 from util import get_url, check_valid_url
 
 
 def generate_script(header_dict, details_dict, searchString=None):
+    # TODO: Docstring and comments.
     url = get_url(header_dict['Host'])
     if not check_valid_url(url):
-        print(
-            "Please enter a valid URL with correct domain name and try again ")
-        sys.exit(0)
-
+        print("Please enter a valid URL with correct domain name and try again ")
+        sys.exit(-1)
     if details_dict['data']:
         details_dict['data'] = '"' + str(details_dict['data']) + '"'
-
     encoding_list = ['HEAD', 'OPTIONS', 'GET']
-
-    if details_dict['data'] and (details_dict['method'].strip()
-                                 in encoding_list):
-        print "Encoding URL"
+    if details_dict['data'] and (details_dict['method'].strip() in encoding_list):
+        print("Encoding URL")
         encoded_data = quote(details_dict['data'], '')
         url = url + encoded_data
         header_dict['Host'] = url
         details_dict['data'] = None
-
     if searchString:
         try:
             if 'proxy' not in details_dict:
                 skeleton_code = '''\
 #!/usr/bin/python
+import re
 from tornado.httpclient import HTTPRequest, HTTPClient
 from termcolor import colored
-import re
+
 
 def main():
     headers, url, method = ''' + str(header_dict) + ''', "''' + url + '''" , "''' + details_dict['method'].strip() + '''"
@@ -55,14 +53,13 @@ def main():
 
 if __name__ == '__main__':
     main()
-
                 '''
             else:
                 skeleton_code = '''\
 #!/usr/bin/python
+import re
 from tornado.httpclient import HTTPRequest, HTTPClient
 from termcolor import colored
-import re
 
 
 def main():
@@ -85,12 +82,10 @@ def main():
 if __name__ == '__main__':
     main()
                 '''
-
         except IndexError:
-            print "You haven't given the port Number"
+            print("You haven't given the port Number")
         else:
-            print skeleton_code
-
+            print(skeleton_code)
     else:
         try:
             if 'proxy' not in details_dict:
@@ -108,7 +103,6 @@ def main():
 if __name__ == '__main__':
     main()
                 '''
-
             else:
                 skeleton_code = '''\
 #!/usr/bin/python
@@ -132,9 +126,8 @@ def main():
 if __name__ == '__main__':
     main()
                 '''
-
         except IndexError:
-            print "You haven't given the port Number"
+            print("You haven't given the port Number")
 
         else:
             print(skeleton_code)
