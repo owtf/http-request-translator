609ce5b4d509b542a813fe21773edc1c60026fdb
Tao Sauvage <sauvage.tao@gmail.com>
2015-07-17 21:15:46 +0200

    Quick cleaning of util.py.

--- http_request_translator/util.py
+++ http_request_translator/util.py
@@ -1,11 +1,13 @@
-#!/usr/bin/python
-
-from urlparse import urlparse
 import re
+
+try:
+    from urlparse import urlparse
+except ImportError:
+    from urllib.parse import urlparse
 
 
 def check_valid_url(test_url):
-
+    # TODO: Docstring and comments.
     parsed_url = urlparse(test_url)
     host_address = ''
     if ":" in parsed_url[1]:
@@ -13,7 +15,8 @@ def check_valid_url(test_url):
     else:
         host_address = parsed_url[1]
     domain_regex = re.compile(
-        r'(?:(?:[A-Z](?:[A-Z-]{0,61}[A-Z])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|)', re.IGNORECASE)
+        r'(?:(?:[A-Z](?:[A-Z-]{0,61}[A-Z])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|)',
+        re.IGNORECASE)
     domain_match = domain_regex.match(host_address)
     if domain_match:
         if not domain_match.group():
@@ -24,8 +27,8 @@ def check_valid_url(test_url):
 
 
 def get_url(host):
-    port_protocol = {443: 'https', 22: 'ssh',
-                     21: 'ftp', 20: 'ftp', 113: 'irc', 80: 'http'}
+    # TODO: Docstring and comments.
+    port_protocol = {443: 'https', 22: 'ssh', 21: 'ftp', 20: 'ftp', 113: 'irc', 80: 'http'}
     url = str(host)
     try:
         port = url.split(':', 2)[1]
@@ -34,7 +37,6 @@ def get_url(host):
             prefix = str(port_protocol[int(port.strip())]) + "://"
         else:
             prefix = "http://"
-
     except IndexError:
         prefix = "http://"
     url = prefix + url
