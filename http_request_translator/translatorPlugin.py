4ed0b765bcb32ece7e5a3ff4f72f45f4cbeb25d6
Arun Sori <arunsori94@gmail.com>
2015-07-17 09:50:59 +0530

    Add setup.py to support installation as a package
    Signed-off-by: Arun Sori <arunsori94@gmail.com>

--- http_request_translator/translatorPlugin.py
+++ http_request_translator/translatorPlugin.py
@@ -9,9 +9,9 @@ def pluginManager(script_list, parsed_tuple, searchString=None):
 	for x in range(0, len(script_list)) :
 		string = script_list[x].lower() + "Scripts"
 
-		try :
-			__import__(string).generate_script(parsed_tuple[0],\
-			parsed_tuple[1], searchString)
+        try:
+            __import__(string, globals={"__name__": __name__}).generate_script(parsed_tuple[0],
+                                                                               parsed_tuple[1], searchString)
 
 		except ImportError :
 			print("The support for generating the\
