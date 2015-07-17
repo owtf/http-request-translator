84614ed4ae0734067f738adb77d15e2c05f9fba0
Tao Sauvage <sauvage.tao@gmail.com>
2015-07-17 21:33:03 +0200

    Quick cleaning of default.py

--- http_request_translator/default.py
+++ http_request_translator/default.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python
-
-
 def generate_script(header_dict, details_dict):
+    # TODO: Docstring and comments.
     ''' This method contains the skeleton code of Curl Command '''
     skeleton_code = "<Insert the skeleton_code here>"
     ''' Take in the values from the parsed_dictionary and then add the parsed values to this to make a basic script
