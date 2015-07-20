64f037324ca1895111dda7da92ef3e3315dd4d39
root <root@localhost.localdomain>
2015-07-20 11:13:41 -0400

    Renamed functions to be more descriptive and cleaned up some parsing.

--- http_request_translator/bash_script.py
+++ http_request_translator/bash_script.py
@@ -6,24 +6,25 @@ import re
 def generate_script(header_dict, details_dict, searchString=None):
     # TODO: Docstring and comments.
     method = details_dict['method'].strip()
+    host = details_dict['Host'].strip()
     headers = str(header_dict)
-    a = re.sub(r"{|}", "", headers)
-    b = re.sub(r"': '", " : ", a)
-    c = re.sub(r"', '", "\" --header \"", b)
-    d = re.sub(r"\'", "\"", c)
-    e = re.sub(r"\"Host :", "", d)
-    formattedHeaders = re.sub(r"\"", "", e, 1)
+    stripCurlyBrackets = re.sub(r"{|}", "", headers)
+    stripUnwantedSingleQuotes = re.sub(r"': '", " : ", stripCurlyBrackets)
+    addHeaders = re.sub(r"', '", "\" --header \"", stripUnwantedSingleQuotes)
+    addLeadingDoubleQuote = re.sub(r"\'", "\"", addHeaders)
+    stripHost = re.sub(r'\"Host\s?[^A-Z]*(?=\")', "", addLeadingDoubleQuote)
+    formattedHeaders = re.sub(r"\"", "", stripHost, 1)
     if searchString:
         try:
             if 'proxy' not in details_dict:
                     skeleton_code = '''\
 #!/usr/bin/env bash
-curl -s --request ''' + method + formattedHeaders + ''' --include | egrep --color ''' + "'" + searchString + '''|$'
+curl -s --request ''' + method + ''' ''' + host + ''' "''' + formattedHeaders + ''' --include | egrep --color ''' + "'" + searchString + '''|$'
                 '''
             else:
                 skeleton_code = '''\
 #!/usr/bin/env bash
-curl -x ''' + details_dict['proxy'] + ''' -s --request ''' + method + formattedHeaders + ''' --include | egrep --color ''' + "'" + searchString + '''|$'
+curl -x ''' + details_dict['proxy'] + ''' -s --request ''' + method + ''' ''' + host + ''' "''' + formattedHeaders + ''' --include | egrep --color ''' + "'" + searchString + '''|$'
                 '''
         except IndexError:
             print("You haven't given the port Number")
@@ -34,12 +35,12 @@ curl -x ''' + details_dict['proxy'] + ''' -s --request ''' + method + formattedH
             if 'proxy' not in details_dict:
                 skeleton_code = '''\
 #!/usr/bin/env bash
-curl -s --request '''+method+formattedHeaders+''' --include
+curl -s --request ''' + method + ''' ''' + host + ''' "''' + formattedHeaders + ''' --include
                 '''
             else:
                 skeleton_code = '''\
 #!/usr/bin/env bash
-curl -x '''+details_dict['proxy']+''' -s --request '''+method+formattedHeaders+''' --include
+curl -x '''+details_dict['proxy']+''' -s --request ''' + method + ''' ''' + host + ''' "''' + formattedHeaders + ''' --include
                 '''
         except IndexError:
             print("You haven't given the port Number")
