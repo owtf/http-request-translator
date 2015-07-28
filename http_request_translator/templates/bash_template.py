4753dffc6a1672dfa99a5a5da8f082d6554bbb8f
Arun Sori <arunsori94@gmail.com>
2015-07-28 22:25:41 +0530

    Fix whitespace in bash script code template
    Signed-off-by: Arun Sori <arunsori94@gmail.com>

--- http_request_translator/templates/bash_template.py
+++ http_request_translator/templates/bash_template.py
@@ -1,13 +1,13 @@
 begin_code = """
 #!/usr/bin/env bash
-curl -s --request """
+curl"""
 
 request_header = """ --header "{header} : {header_value}" """
 
-code_search = " | egrep --color ' {search_string} |$' "
+code_search = " | egrep --color ' {search_string} |$'"
 
-code_simple = "{method} {url} {headers} --include "
+code_simple = " -s --request {method} {url} {headers} --include"
 
-proxy_code = "-x {proxy}"
+proxy_code = " -x {proxy}"
 
-body_code = " --data '{body}' "
+body_code = " --data '{body}'"
