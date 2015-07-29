f35147dde3fd0328b33b41ac687491fea2a9df0e
Arun Sori <arunsori94@gmail.com>
2015-07-29 23:44:39 +0530

    Update php script support, move template code to separate dir
    Signed-off-by: Arun Sori <arunsori94@gmail.com>

--- http_request_translator/php_script.py
+++ http_request_translator/php_script.py
@@ -1,69 +1,99 @@
 from __future__ import print_function
 
+import re
+try:
+    from urllib import quote
+except ImportError:
+    from urllib.parse import quote
+
+from url import get_url, check_valid_url
+from templates import php_template
+
 
 def generate_script(header_dict, details_dict, searchString=None):
-    # TODO: Docstring and comments.
-    method = details_dict['method'].strip()
-    host = details_dict['Host']
-    headers = str(header_dict)
-    if searchString:
-        try:
-            if 'proxy' not in details_dict:
-                    skeleton_code = '''\
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => '''+"'"+host+"',"+'''
-));
-$response = curl_exec($curl);
-curl_close($curl);
-return $response
-?>'''
-            else:
-                skeleton_code = '''\
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => '''+"'"+host+"',"+'''
-));
-$response = curl_exec($curl);
-curl_close($curl);
-return $response
-?>'''
+    """Generate the php script for the passed request.
 
-        except IndexError:
-            print("You haven't given the port Number")
-        else:
-            return skeleton_code
-    else:
+    :param dict header_dict: Header dictionary containing fields like 'Host','User-Agent'.
+    :param dict details_dict: Request specific details like body and method for the request.
+    :param str searchString: String to search for in the response to the request. By default remains None.
+
+    :raises ValueError: When url is Invalid
+
+    :return: A combined string of generated code
+    :rtype:`str`
+    """
+    url = get_url(header_dict['Host'], details_dict['pre_scheme'])
+    method = details_dict['method'].strip()
+    if details_dict['path'] != "":
+        url += details_dict['path']
+    if not check_valid_url(url):
+        raise ValueError("Invalid URL")
+    encoding_list = ['HEAD', 'OPTIONS', 'GET']
+    if details_dict['data'] and (details_dict['method'].strip() in encoding_list):
+        encoded_data = quote(details_dict['data'], '')
+        url = url + encoded_data
+    skeleton_code = php_template.begin_code.format(url=url)
+    skeleton_code += generate_request_headers(header_dict)
+    if method == "GET":
+        skeleton_code += generate_req_code(details_dict, searchString)
+    elif method == "POST":
+        skeleton_code += php_template.post_request.format(body=generate_body_code(details_dict['data']))
+        skeleton_code += generate_req_code(details_dict, searchString)
+
+    return skeleton_code
+
+
+def generate_request_headers(header_dict):
+    """Place the request headers in php script from header dictionary.
+
+    :param dict header_dict: Header dictionary containing fields like 'Host','User-Agent'.
+
+    :return: A string of php code which places headers in the request.
+    :rtype:`str`
+    """
+    skeleton = ""
+    for key, value in header_dict.items():
+        skeleton_ = php_template.request_header.format(header=str(key), header_value=str(value))
+        skeleton += skeleton_
+    return skeleton
+
+
+def generate_req_code(details_dict, searchString):
+    """Generate php code for the body and proxy specific parts of the request.
+
+    :param dict details_dict: Dictionary of request details like proxy,data etc.
+    :param str searchString: String to search for in response of the request
+
+    :raises IndexError: If proxy provided is invalid
+
+    :return: A string of combined php code for specific proxy and searchString if one passed
+    :rtype: `str`
+    """
+    skeleton = ""
+    if 'proxy' in details_dict:
         try:
-            if 'proxy' not in details_dict:
-                skeleton_code = '''\
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => '''+"'"+host+"',"+'''
-));
-$response = curl_exec($curl);
-curl_close($curl);
-return $response
-?>'''
-            else:
-                skeleton_code = '''\
-<?php
-$curl = curl_init();
-curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => '''+"'"+host+"',"+'''
-));
-$response = curl_exec($curl);
-curl_close($curl);
-return $response
-?>'''
+            proxy_host, proxy_port = details_dict['proxy'].split(':')
         except IndexError:
-            print("You haven't given the port Number")
-        else:
-            return skeleton_code
+            raise IndexError("Proxy provided is invalid.")
+        skeleton += php_template.proxy_code.format(proxy_host=proxy_host, proxy_port=proxy_port)
+    skeleton += php_template.req_code
+    if searchString:
+        searchString = re.sub("'", "\\'", searchString)
+        skeleton += php_template.search_code.format(search_string=searchString)
+    else:
+        skeleton += php_template.non_search_code
+    return skeleton
+
+
+def generate_body_code(body):
+    """Generate string for body part of the request.
+
+    :param str body:Passed body of the request
+
+    :return: A formatted string of body code
+    :rtype:`str`
+
+    """
+    # Escape single quotes , double quotes are good here
+    body = re.sub(r"'", "\\'", body)
+    return str(body)
---
+++ http_request_translator/templates/php_template.py
@@ -0,0 +1,57 @@
+begin_code = """
+<?php
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, '{url}');
+// Set so curl_exec returns the result instead of outputting it.
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$headers = array();
+"""
+
+request_header = """
+$headers[] = "{header}: {header_value}";
+"""
+
+post_request = """
+$content = '{body}';
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+"""
+
+proxy_code = """
+curl_setopt($ch, CURLOPT_PROXYPORT, {proxy_port});
+curl_setopt($ch, CURLOPT_PROXY, {proxy_host});
+"""
+
+req_code = """
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+ print curl_error($ch);
+} else {
+ curl_close($ch);
+}
+"""
+search_code = """
+print $response;
+$string = '{search_string}';
+// Blindly copied from http://stackoverflow.com/questions/10778318/test-if-a-string-is-regex
+// Checks if the passed string is a regex or a simple string
+if( preg_match("/^\/.+\/[a-z]*$/i",$string)) {{
+    if (preg_match($string, $response, $match)) {{
+        print 'Found a match!';
+    }}
+}}
+else {{
+    if (strpos($response,$string) !== false) {{
+        print 'Found a match!';
+    }}
+}}
+
+?>
+"""
+
+non_search_code = """
+print $response;
+?>
+"""
