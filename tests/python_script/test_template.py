3578f2bd2a13615d24ca6ff436766045d7bd8d43
Arun Sori <arunsori94@gmail.com>
2015-08-07 20:16:07 +0530

    Add tests for python script

---
+++ tests/python_script/test_template.py
@@ -0,0 +1,80 @@
+generated_search_string = """
+    try:
+        c.perform()
+    except pycurl.error, error:
+        print('An error occurred: ', error)
+    c.close()
+
+    body = buffer.getvalue()
+    # Body is a string on Python 2 and a byte string on Python 3.
+    # If we know the encoding, we can always decode the body and
+    # end up with a Unicode string.
+    response = body.decode('iso-8859-1')
+
+    match = re.findall(r'hello3131\\'you\\'are\\'awesome', str(response))
+    try:
+        from termcolor import colored
+        lib_available = True
+    except ImportError:
+        lib_available = False
+    if match:
+        for item in match:
+            if lib_available:
+                replace_string = colored(match[x], 'green')
+                response = re.sub(match[x], replace_string, str(response))
+            else:
+                print("Matched item: ",item)
+
+    print(response)
+
+
+if __name__ == '__main__':
+    main()
+"""
+
+generated_script = """
+#!/usr/bin/python
+from __future__ import print_function
+import re
+import pycurl
+try:
+    from io import BytesIO
+except ImportError:
+    from StringIO import StringIO as BytesIO
+
+def main():
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, 'https://google.com/robots.txt')
+    c.setopt(c.WRITEDATA, buffer)
+    c.setopt(c.HTTPHEADER, ['Host: google.com'])
+    # for verbosity
+    c.setopt(c.VERBOSE, True)
+    # Follow redirects
+    c.setopt(c.FOLLOWLOCATION, True)
+    # For older PycURL versions:
+    #c.setopt(c.WRITEFUNCTION, buffer.write)
+
+    c.setopt(pycurl.SSL_VERIFYPEER, 1)
+    c.setopt(pycurl.SSL_VERIFYHOST, 2)
+    # If providing updated certs
+    # c.setopt(pycurl.CAINFO, "/path/to/updated-certificate-chain.crt")
+
+    try:
+        c.perform()
+    except pycurl.error, error:
+        print('An error occurred: ', error)
+    c.close()
+
+    body = buffer.getvalue()
+    # Body is a string on Python 2 and a byte string on Python 3.
+    # If we know the encoding, we can always decode the body and
+    # end up with a Unicode string.
+    response = body.decode('iso-8859-1')
+
+    print(response)
+
+
+if __name__ == '__main__':
+    main()
+"""
---
+++ tests/python_script/tests.py
@@ -0,0 +1,36 @@
+import unittest
+
+from http_request_translator import python_script
+import test_template
+
+
+class TestPythonTranslator(unittest.TestCase):
+
+    def test_generate_search_code(self):
+        search_string = "hello3131'you'are'awesome"
+        result = python_script.generate_search_code(search_string)
+        self.assertEqual(result, test_template.generated_search_string, 'Invalid generation of python search code')
+
+    def test_generate_proxy_code(self):
+        details_dict = {'proxy_port': '2223', 'proxy_host': 'http://xyz.com'}
+        result = python_script.generate_proxy_code(details_dict)
+        string_generated = """
+    c.setopt(c.PROXY, 'http://xyz.com:2223')
+"""
+        self.assertEqual(result, string_generated, 'Invalid generation of python proxy code')
+
+    def test_generate_script(self):
+        header_list = ['Host: google.com']
+        details_dict = {'protocol': 'HTTP',
+                        'pre_scheme': 'https://',
+                        'Host': 'google.com',
+                        'version': '1.1',
+                        'path': '/robots.txt',
+                        'method': 'GET',
+                        'data': ''
+                        }
+        result = python_script.generate_script(header_list, details_dict)
+        self.assertEqual(result, test_template.generated_script, 'Invalid generation of Python script')
+
+if __name__ == '__main__':
+    unittest.main()
