edb62549e50f707554d724c80e928731a12af07d
Dhruv Agarwal <dhruvagga@gmail.com>
2016-05-22 21:11:55 +0530

    delete exceptions

--- http_request_translator/exceptions.py
+++
@@ -1,4 +0,0 @@
-class NoRequestProvided(Exception):
-    def __init__(self):
-        message = "Input a valid raw request and try again.\nOr use Interactive mode instead"
-        super(NoRequestProvided, self).__init__(message)
