a56f3507ad5bc22ca753d1120f5882b8c583aa26
Tao Sauvage <sauvage.tao@gmail.com>
2017-01-20 00:08:47 +0900

    [tests] Additional tests for check_valid_url.

--- tests/test_url.py
+++ tests/test_url.py
@@ -13,13 +13,17 @@ class TestURL(unittest.TestCase):
         self.assertTrue(url.check_valid_url("http://[::1]"))
         self.assertTrue(url.check_valid_url("https://[::1]:443"))
         self.assertTrue(url.check_valid_url("http://" + 'a' * 63 + ".com"))
+        self.assertTrue(url.check_valid_url("http://192.168.1.1"))
 
         self.assertFalse(url.check_valid_url("://github.com/"))
         self.assertFalse(url.check_valid_url("https://::1:abcd"))
         self.assertFalse(url.check_valid_url("http://" + 'a' * 64 + ".com"))
+        self.assertFalse(url.check_valid_url("http://192.999.1.1"))
+        self.assertFalse(url.check_valid_url("https://[::1]:invalid"))
+        self.assertFalse(url.check_valid_url("https://[::1]:9999999999"))
 
         # Regression for https://github.com/owtf/http-request-translator/issues/44
-        self.assertTrue(url.check_valid_url("https://www.cmd5.com"))
+        self.assertTrue(url.check_valid_url("http://www.cmd5.com"))
         self.assertTrue(url.check_valid_url("https://www-cmd5.com"))
 
     ###
